5b9b78a6511ab14931ae0dba2cb1b86c599b919c
Jan Mrázek <email@honzamrazek.cz>
2016-06-16 09:51:37 +0200

    Add Tagged type

---
+++ examples/tagged/main.cpp
@@ -0,0 +1,43 @@
+#include <atoms/type/tagged.h>
+#include <atoms/numeric/vector.h>
+#include <iostream>
+#include <string>
+
+// This file is part of 'Atoms' library - https://github.com/yaqwsx/atoms
+// Author: Jan 'yaqwsx' Mrázek
+
+
+// This example demonstrates usage of Tagged class. This class allows user
+// to keep a tag with an arbitrary data structure
+
+using namespace atoms;
+
+template <class V>
+std::string to_str(V v) {
+    return "[" + std::to_string(v.x) + ", " + std::to_string(v.y) + "]";
+}
+
+template <class V>
+std::string to_str_v(V v) {
+    return "[" + std::to_string(v.x) + ", " + std::to_string(v.y) +
+        ", " + std::to_string(v.tag) + "]";
+}
+
+int main() {
+    Tagged<Vector2D<int>> v(5, 2), u(4, 2); // Vectors with no tag
+    std::cout << "v = " << to_str(v) << ", u = " << to_str(u) << "\n";
+
+    auto t = v;
+    Tagged<Vector2D<int>> tt(v);
+    std::cout << "assign = " << to_str(t) << ", copy = " << to_str(tt) << "\n";
+
+    std::cout << "u + v =" << to_str(u + v) << "\n";
+
+    using V2T = Tagged<Vector2D<int>, int, OperatorMerge<int>>;
+    auto a = V2T::make_tagged(42, 1, 1);
+    auto b = V2T::make_tagged(10, -1, -1);
+    std::cout << "a = " << to_str_v(a) << ", b = " << to_str_v(b) << "\n";
+    std::cout << "a + b = " << to_str_v(a + b) << "\n";
+
+    return 0;
+}
---
+++ include/atoms/type/tagged.h
@@ -0,0 +1,85 @@
+#pragma once
+
+// This file is part of 'Atoms' library - https://github.com/yaqwsx/atoms
+// Author: Jan 'yaqwsx' Mrázek
+
+#include <tuple>
+#include <cassert>
+#include <iostream>
+
+namespace atoms {
+
+enum class Operator {
+    ADD, SUBTRACT, MULTIPLY, DIVIDE, EQUAL
+    // ToDo: Add other operators
+};
+
+struct NoTagMerge {
+    static std::tuple<> merge(Operator, const std::tuple<>&, const std::tuple<>&) {
+        return {};
+    }
+
+    static bool equal(const std::tuple<>&, const std::tuple<>&) {
+        return true;
+    }
+};
+
+template <class Type>
+struct OperatorMerge {
+    static Type merge(Operator op, const Type& a, const Type& b) {
+        switch(op) {
+            case Operator::ADD:
+                return a + b;
+            case Operator::SUBTRACT:
+                return a - b;
+            case Operator::MULTIPLY:
+                return a * b;
+            case Operator::DIVIDE:
+                return a / b;
+            default:
+                assert(false && "Unsupported operation");
+        }
+    }
+
+    static bool equal(const Type& a, const Type& b) {
+        return a == b;
+    }
+};
+
+// Adds a tag to an arbitrary class or structure, keeping operator definitions
+// and providing tag merge procedure
+template <class Base, class TagType = std::tuple<>, class TagMerge = NoTagMerge>
+struct Tagged : public Base {
+    using Base::Base;
+
+    Tagged& operator=(const Base& b) { Base::oeprator=(b); }
+    Tagged(const Base& b) : Base(b) {}
+
+    template <class... Args>
+    static Tagged make_tagged(const TagType& t, Args... args) {
+        Tagged res{args...};
+        res.tag = t;
+        return res;
+    }
+
+    TagType tag;
+};
+
+#define GENERATE_TAGGED_OPERATOR(op, opcode) \
+template <class Base1, class Base2, class TagType, class TagMerge> \
+auto operator op (const Tagged<Base1, TagType, TagMerge>& a, const Tagged<Base2, TagType, TagMerge>& b) \
+    -> Tagged<decltype(std::declval<Base1>() op std::declval<Base2>()), TagType, TagMerge> \
+{ \
+    using  ResType = Tagged<decltype(std::declval<Base1>() op std::declval<Base2>()), TagType, TagMerge>; \
+    ResType res = (*static_cast<const Base1*>(&a)) op (*static_cast<const Base2*>(&b)); \
+    res.tag = TagMerge::merge(opcode, a.tag, b.tag); \
+    return res; \
+}
+
+GENERATE_TAGGED_OPERATOR(-, Operator::SUBTRACT);
+GENERATE_TAGGED_OPERATOR(+, Operator::ADD);
+GENERATE_TAGGED_OPERATOR(*, Operator::MULTIPLY);
+GENERATE_TAGGED_OPERATOR(/, Operator::DIVIDE);
+// ToDo: Other operators
+
+} // namespace atoms
