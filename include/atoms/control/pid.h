c7111bcc9a103871170e6c7a7ee972ec8580a4e1
Jan Mrázek <email@honzamrazek.cz>
2017-07-02 13:34:57 +0200

    Fix Pid::Step type mismatch

--- include/atoms/control/pid.h
+++ include/atoms/control/pid.h
@@ -14,51 +14,51 @@ public:
 		T p, i, d;
 		T bottom, top;
 	};
-    
+
     struct State {
         Value<T, Clamped> integrator;
         T last_input;
-        
+
         State(const T& bottom, const T& top)
             : integrator(0, { bottom, top }), last_input(0) { }
     };
-    
+
     Pid(const Config& params)
         : params(params), state(params.bottom, params.top) {}
-    
+
     Config get_params() const {
         return params;
     }
-    
+
     void set_params(Config& p) {
         params = p;
         reset();
     }
-    
+
     State get_state() const {
         return state;
     }
-    
+
     void reset() {
         state = State(params.bottom, params.top);
     }
-    
+
     T step(T input, T desired_value) {
         T error(desired_value - input);
         state.integrator = state.integrator.get() + params.i * error;
-            
+
         Value<T, Clamped> output(params.p * error + state.integrator
             - params.d * (input - state.last_input),
             { params.bottom, params.top });
-            
+
         state.last_input = input;
-        
-        return output;            
+
+        return output;
     }
-    
+
     T step(T input, T desired_value, T time_step) {
         T error(desired_value - input);
-        state.integrator += params.i * error * time_step;
+        state.integrator = state.integrator.get() + params.i * error * time_step;
 
         Value<T, Clamped> output(params.p * error * time_step + state.integrator
             - params.d * (input - state.last_input) * time_step,
